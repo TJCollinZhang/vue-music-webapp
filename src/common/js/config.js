3cc876c5dcb1a512955b48565ca39ef17ccad7ee
Zhang.Cong <1084823834@qq.com>
2019-12-03 00:31:44 +0800

    modify the baseUrl

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,4 +1,5 @@
-export const HOST = 'http://localhost:3000'
+
+export const HOST = process.env.NODE_ENV === 'production' ? 'https://music.api.collinjs.site':'http://localhost:3000'
 export const ERR_OK = 200
 
 export const playMode = {
